cfa5aa5dac61f5c6e7977b03976816f03fddff38
erik-lance <eriklance@gmail.com>
2023-05-11 22:49:49 +0800

    Changed filenames

--- src/main/java/com/weatherapp/weatherapp/Controller.java
+++ src/main/java/com/weatherapp/weatherapp/Controller.java
@@ -3,7 +3,7 @@ package com.weatherapp.weatherapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
