6b53f1ca7aa98d7c4d1bf6f38eb0b548b39136f4
erik-lance <eriklance@gmail.com>
2023-05-11 21:37:23 +0800

    Initial Files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.weatherapp.weatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.weatherapp.weatherapp to javafx.fxml;
+    exports com.weatherapp.weatherapp;
+}
