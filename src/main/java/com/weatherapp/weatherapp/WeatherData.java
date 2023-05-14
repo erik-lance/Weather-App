b6436e7209adc14420ccb1e586e54fb05bfae82a
erik-lance <eriklance@gmail.com>
2023-05-14 19:59:52 +0800

    Removed redundant constructor for record

--- src/main/java/com/weatherapp/weatherapp/WeatherData.java
+++ src/main/java/com/weatherapp/weatherapp/WeatherData.java
@@ -2,18 +2,6 @@ package com.weatherapp.weatherapp;
 
 public record WeatherData(String city, String country, String description, String temperature, String humidity,
                           String pressure, String windSpeed, String windDirection) {
-    public WeatherData(String city, String country, String description,
-                       String temperature, String humidity, String pressure,
-                       String windSpeed, String windDirection) {
-        this.city = city;
-        this.country = country;
-        this.description = description;
-        this.temperature = temperature;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        this.windSpeed = windSpeed;
-        this.windDirection = windDirection;
-    }
 
     @Override
     public String toString() {
